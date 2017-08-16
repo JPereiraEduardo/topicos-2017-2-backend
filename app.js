f2aa2f4df70c3ebaa33fa2ff8d6e1cbfd9ffd144
EduardoPereira <kasopeas@hotmail.com>
2017-08-16 20:59:32 -0300

    Adicionamos metodos de alteração e de exclusão do Todo. (Quarta-Feira 16/08) eduardo_santiago_@outlook.com

--- app.js
+++ app.js
@@ -4,8 +4,6 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
 var todos = require('./routes/todos');
 
 // Inicia os express (middlewares)
@@ -20,8 +18,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 // Definição dos arquivos de rotas
-app.use('/', index);
-app.use('/users', users);
 app.use('/todos',todos);
 
 // Captura os erros 404
--- routes/todos.js
+++ routes/todos.js
@@ -9,39 +9,76 @@ todos.get('/', function (req, res, next) {
     console.log("Recebemos uma requisição GET");
 
     res.status(200).json(todosList);
-
 });
 
 todos.get('/:todoId', function (req, res, next) {
-
     console.log("Recebemos uma requisição GET");
 
-    var todosFiltered =
-        todosList.filter(function (todo, index) {
-            return todo.id === parseInt(req.params.todoId);
-        });
+    var todo = findTodoById(req.params.todoId);
 
-    if (todosFiltered.lenght === 0) {
-        res.status(404).send();
+    if (todo) {
+        res.status(200).json(todo);
     } else {
-        res.status(200).json(todosFiltered[0]);
+        res.status(404).send();
     }
 
-
 });
-
-
 
 todos.post('/', function (req, res, next) {
-
     console.log("Recebemos uma requisição POST");
 
+    //recebe os dados da Todo
     var todo = req.body;
     todo.id = todoIndex++;
+    todo.completed = false;
+
+    //insere a todo no "banco de dados"
     todosList.push(todo);
 
-    res.status(200).send();
+    res.status(201).send();
 });
+
+todos.put('/:todoId', function (req, res, next) {
+    var todo = findTodoById(req.params.todoId);
+
+    if (todo) {
+        //alteração
+        todo.title = req.body.title;
+        todo.description = req.body.description;
+        todo.completed = req.body.completed;
+
+        res.status(200).json(todo);
+    } else {
+        res.status(404).send();
+    }
+});
+
+todos.delete('/:todoId', function(req, res, next){
+
+    // busco o indice da Todo no "banco de dados"
+    var index = todosList.findIndex(function(todo, index){
+        return todo.id === parseInt(req.params.todoId);
+    });
+
+    if(index >= 0){
+    // removo a Todo do "banco de dados"
+    todosList.splice(index, 1);
+
+    res.status(200).send();
+    }else{
+        res.status(404).send();
+    }
+});
+
+function findTodoById(todoId) {
+    var todosFiltered =
+        todosList.filter(function (todo, index) {
+            return todo.id === parseInt(todoId);
+        });
+    if (todosFiltered.length > 0)
+        return todosFiltered[0];
+    return null;
+}
 
 
 // Definições das rotas
